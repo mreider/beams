d5fdaabd436f0ea3f426ae03f570c67c21df080c
Matthew Reider <matthew.reider@dynatrace.com>
2021-10-09 18:42:45 +0200

    better sizing

--- app/db.js
+++ app/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb:27017", { 
+mongoose.connect("mongodb://mongodb:27017", { 
      useNewUrlParser: true,
      useUnifiedTopology: true
 })
