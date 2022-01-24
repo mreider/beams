00d3432195073a96cdc77a23dadc2d6ce938342b
Matt Reider <mreider@gmail.com>
2022-01-24 15:36:36 +0100

    bug fix

--- app/confirmationServer.js
+++ app/confirmationServer.js
@@ -9,24 +9,9 @@ const logger = winston.createLogger({
     ]
 });
 
-function blockCpuFor(ms) {
-	var now = new Date().getTime();
-	var result = 0
-	while(shouldRun) {
-		result += Math.random() * Math.random();
-		if (new Date().getTime() > now +ms)
-			return;
-	}	
-}
-
-function start() {
-	shouldRun = true;
-	blockCpuFor(1000*desiredLoadFactor);
-	setTimeout(start, 1000* (1 - .5));
-}
-
 app.post('/confirmation', (req, res) => {
     res.sendStatus(200);
+	logger.info('confirmed');
     start();
 })
 
